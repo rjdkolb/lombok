f737a6a42ea5dc60d0f113e31945bcb9a167c887
Richard Kolb <RichardK2@discovery.co.za>
2015-10-07 12:48:13 +0200

    typeo

--- src/main/java/io/r3k/lombok/App.java
+++ src/main/java/io/r3k/lombok/App.java
@@ -2,7 +2,7 @@ package io.r3k.lombok;
 
 public class App {
     public static void main(String[] args) {
-        MyBean b = new MyBean(x, 0);
+        MyBean b = new MyBean("x", 0);
         b.getX();
         b.getY();
     }
