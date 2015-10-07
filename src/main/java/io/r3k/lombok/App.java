2743a35f1d84cb3984ec47e2652eb34273cc966d
Richard Kolb <RichardK2@discovery.co.za>
2015-10-07 12:45:20 +0200

    init commit

---
+++ src/main/java/io/r3k/lombok/App.java
@@ -0,0 +1,9 @@
+package io.r3k.lombok;
+
+public class App {
+    public static void main(String[] args) {
+        MyBean b = new MyBean(x, 0);
+        b.getX();
+        b.getY();
+    }
+}
---
+++ src/main/java/io/r3k/lombok/MyBean.java
@@ -0,0 +1,13 @@
+package io.r3k.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyBean {
+
+    String x;
+    int y;
+    
+}
